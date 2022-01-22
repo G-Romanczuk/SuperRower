d0b04879dfc0c5a518eb485ca5b5859da4f2ecce
G-Romanczuk <79995250+G-Romanczuk@users.noreply.github.com>
2022-01-22 05:31:49 +0100

    SuperRower 1
    formularz, walidacja, odpowiedź

---
+++ SuperRower/SuperRower/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SuperRower.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperRower.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public ViewResult ContactForm()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ViewResult ContactForm(Contact contact)
+        {
+            //TODO utrwalić dane w systemie
+            if (ModelState.IsValid)
+            {
+                return View("ConfirmContact", contact);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ SuperRower/SuperRower/Models/Contact.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SuperRower.Models
+{
+    public class Contact
+    {
+
+        [Required(ErrorMessage = "Proszę podać imię Klienta!")]
+        [StringLength(20, ErrorMessage = "Proszę podać poprawne imię Klienta!")]
+        public string NameCustomer { get; set; }
+        [Required(ErrorMessage = "Proszę podać Nazwisko Klienta!")]
+        [StringLength(30, ErrorMessage = "Proszę podać poprawne nazwisko Klienta!")]
+        public string LastNameCustomer { get; set; }
+        [Required(ErrorMessage = "Proszę podać  numer telefonu Klienta!")]
+        [Phone(ErrorMessage = "Proszę podać poprawny numer telefonu Klienta!")]
+        public string TelCustomer { get; set; }
+        [Required(ErrorMessage = "Proszę podać nazwę miasta Klienta!")]
+        [MinLength(length: 2, ErrorMessage = "Proszę podać poprawną nazwę miasta Klienta!")]
+        public string CityAdressCustomer { get; set; }
+        [Required(ErrorMessage = "Proszę podać poprawną nazwę ulicy Klienta!")]
+        [MaxLength(length: 50, ErrorMessage = "Proszę podać poprawną nazwę ulicy Klienta!")]
+        public string StreetAdressCustomer { get; set; }
+        [Required(ErrorMessage = "Proszę podać numer budynku Klienta!")]
+        [Range(1, 3000, ErrorMessage = "Proszę podać poprawny numer budynku Klienta!")]
+        public string BuildingAdressCustomer { get; set; }
+        [Range(1, 300, ErrorMessage = "Proszę podać poprawny numer mieszkania Klienta!")]
+        public string ApartmentAdressCustomer { get; set; }
+        [Required(ErrorMessage = "Proszę podać kod pocztowy Klienta!")]
+        [RegularExpression("[0-9]{2}-[0-9]{3}", ErrorMessage = "Proszę podać poprawny kod pocztowy Klienta!")]
+        public string KodCustomer { get; set; }
+        public string Model { get; set; }
+        [Required(ErrorMessage = "Proszę podać datę wypożyczenia Roweru!")]
+        public string StartDate { get; set; }
+        [Required(ErrorMessage = "Proszę podać datę oddania Roweru!")]
+        public string EndDate { get; set; }
+
+        
+       
+
+
+
+    }
+
+}
