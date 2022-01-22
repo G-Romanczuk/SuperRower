82994fae5bbca455eb1df7133b5d2f3d53322930
G-Romanczuk <79995250+G-Romanczuk@users.noreply.github.com>
2022-01-22 17:42:17 +0100

    database

--- SuperRower/SuperRower/Startup.cs
+++ SuperRower/SuperRower/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SuperRowerDB;
+using Microsoft.EntityFrameworkCore;
 
 namespace SuperRower
 {
@@ -23,11 +25,13 @@ namespace SuperRower
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SuperRowerDbContext>(options=>options.UseSqlServer("Server=.;Database=SuperRowerDB;Trusted_Connection=True;"));
+          
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -52,6 +56,9 @@ namespace SuperRower
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            var database = serviceProvider.GetService<SuperRowerDbContext>();
+            database.Database.Migrate();
         }
     }
 }
