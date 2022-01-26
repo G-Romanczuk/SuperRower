57fc09b5943cfa5c6061a1e1210f80a872cdb479
G-Romanczuk <79995250+G-Romanczuk@users.noreply.github.com>
2022-01-26 04:39:37 +0100

    Last touches
    Dodaj wiadomość o wymaganiach + logikę - ile do zapłaty po transakcji i jaki rower!

--- SuperRower/SuperRowerDB/Entities/Transaction.cs
+++ SuperRower/SuperRowerDB/Entities/Transaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,25 @@ namespace SuperRowerDB
        
         public int TransactionID { get; set; }
         [ForeignKey("Customer")]
+        [Required(ErrorMessage = "Proszę podać numer Klienta!")]
+        [Range(1, 50, ErrorMessage = "Proszę podać poprawny numer Klienta!")]
         public int CustomerIF { get; set; }
         public virtual Customer Customer { get; set; }
         [ForeignKey("RowerPrice")]
+        [Required(ErrorMessage = "Proszę podać numer roweru!")]
+        [Range(6, 8, ErrorMessage = "Proszę podać poprawny numer roweru!")]
         public int RowerPriceIF { get; set; }
         public virtual RowerPrice RowerPrice { get; set; }
         [ForeignKey("RowerRent")]
+        [Required(ErrorMessage = "Proszę podać numer ceny roweru!")]
+        [Range(6, 8, ErrorMessage = "Proszę podać poprawny numer ceny roweru!")]
         public int RowerRentIF { get; set; }
         public virtual RowerRent RowerRent { get; set; }
+        [Required(ErrorMessage = "Proszę podać datę rozpoczęcia wynajmu!")]
+        [RegularExpression("[0-9]{2}.[0-9]{2}.[0-9]{4}", ErrorMessage = "Proszę podać poprawną datę rozpoczęcia wynajmu dd.mm.rrrr!")]
         public string StartDate { get; set; }
+        [Required(ErrorMessage = "Proszę podać datę Zakończenia wynajmu!")]
+        [RegularExpression("[0-9]{2}.[0-9]{2}.[0-9]{4}", ErrorMessage = "Proszę podać poprawną datę zakończenia wynajmu dd.mm.rrrr!")]
         public string EndDate { get; set; }
     }
 }
