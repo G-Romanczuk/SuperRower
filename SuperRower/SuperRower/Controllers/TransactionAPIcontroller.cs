88cb67d202813dbc4e6308ff7c556da16c4d6191
G-Romanczuk <79995250+G-Romanczuk@users.noreply.github.com>
2022-01-28 18:25:16 +0100

    API

---
+++ SuperRower/SuperRower/Controllers/TransactionAPIcontroller.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperRowerDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperRower.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class API : ControllerBase
+    {
+        private readonly SuperRowerDbContext _context;
+
+        public API(SuperRowerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/API
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Transaction>>> GetTransactions()
+        {
+            return await _context.Transactions.ToListAsync();
+        }
+
+        // GET: api/API/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Transaction>> GetTransaction(int id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return transaction;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AddTransaction(int id, Transaction transaction)
+        {
+            if (id != transaction.TransactionID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(transaction).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TransactionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Transaction>> PostTransaction(Transaction transaction)
+        {
+            _context.Transactions.Add(transaction);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTransaction", new { id = transaction.TransactionID}, transaction);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTransaction(int id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            _context.Transactions.Remove(transaction);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TransactionExists(int id)
+        {
+            return _context.Transactions.Any(e => e.TransactionID == id);
+        }
+    }
+}
